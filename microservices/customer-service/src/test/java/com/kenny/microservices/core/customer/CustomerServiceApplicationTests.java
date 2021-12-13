7b4df3708a0c79c9dcf4e818193ec719fdd7bce4
Kenny <54686876+Kenny-star@users.noreply.github.com>
2021-12-13 01:46:46 -0500

    Setting up web services and environment.

---
+++ microservices/customer-service/src/test/java/com/kenny/microservices/core/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenny.microservices.core.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
